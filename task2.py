12811ea14d17518398ead68600a6d56674f72ea2
Kavish Vijan <kavishvijan461@gmail.com>
2025-06-01 05:53:25 +0530

    Add files via upload
    Demonstrate List Slicing

---
+++ task2.py
@@ -0,0 +1,7 @@
+# Task 2: Demonstrate List Slicing
+list = [1,2,3,4,5,6,7,8,9,10]
+print('Original list;',list)
+a= list[0:5]
+print('Expected First five elements:', a)
+b = a[::-1]
+print('Reversed expected elements:', b)
